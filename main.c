e7815b9c660a630aa2dbf240793aa0374b6c0b74
gregorychernyavskiy <gregory.chernyavskiy@gmail.com>
2025-02-16 17:31:52 -0600

    .

--- main.c
+++ main.c
@@ -3,25 +3,38 @@
 int main(int argc, char *argv[]) {
     srand(time(NULL));
     int load = 0, save = 0;
-    char *saveFileName = NULL; // To store the filename for saving
+    char *saveFileName = NULL;
+    char *loadFileName = NULL;
 
     for (int i = 1; i < argc; i++) {
         if (strcmp(argv[i], "--save") == 0) {
             save = 1;
-            if (i + 1 < argc) { // Check if a filename follows
+            if (i + 1 < argc) { // Ensure there is a filename after --save
                 saveFileName = argv[i + 1];
-                i++; // Skip the next argument since it's the filename
+                i++; // Skip the filename
             } else {
                 printf("Error: Missing filename for --save\n");
                 return 1;
             }
         } else if (strcmp(argv[i], "--load") == 0) {
             load = 1;
+            if (i + 1 < argc) { // Ensure there is a filename after --load
+                loadFileName = argv[i + 1];
+                i++; // Skip the filename
+            } else {
+                printf("Error: Missing filename for --load\n");
+                return 1;
+            }
         }
     }
 
     if (load) {
-        loadDungeon();
+        if (loadFileName) {
+            loadDungeon(loadFileName);
+        } else {
+            printf("Error: No file path specified for loading!\n");
+            return 1;
+        }
     } else {
         emptyDungeon();
         createRooms();
@@ -32,11 +45,10 @@ int main(int argc, char *argv[]) {
     }
 
     printDungeon();
-    printHardness();
 
     if (save) {
         if (saveFileName) {
-            saveDungeon(saveFileName); // Pass the filename to saveDungeon()
+            saveDungeon(saveFileName);
         } else {
             printf("Error: No file path specified for saving!\n");
             return 1;
