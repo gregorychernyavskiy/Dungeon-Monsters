c9cc3393c4cf9d21fb6edce0552eec5ecb68ccdf
gregorychernyavskiy <gregory.chernyavskiy@gmail.com>
2025-02-16 16:59:53 -0600

    .

--- main.c
+++ main.c
@@ -3,10 +3,18 @@
 int main(int argc, char *argv[]) {
     srand(time(NULL));
     int load = 0, save = 0;
+    char *saveFileName = NULL; // To store the filename for saving
 
     for (int i = 1; i < argc; i++) {
         if (strcmp(argv[i], "--save") == 0) {
             save = 1;
+            if (i + 1 < argc) { // Check if a filename follows
+                saveFileName = argv[i + 1];
+                i++; // Skip the next argument since it's the filename
+            } else {
+                printf("Error: Missing filename for --save\n");
+                return 1;
+            }
         } else if (strcmp(argv[i], "--load") == 0) {
             load = 1;
         }
@@ -24,14 +32,15 @@ int main(int argc, char *argv[]) {
     }
 
     printDungeon();
-
     printHardness();
 
     if (save) {
-        char filename[256];
-        strncpy(filename, argv[argc - 1], sizeof(filename) - 1);
-        filename[sizeof(filename) - 1] = '\0';
-        saveDungeon();
+        if (saveFileName) {
+            saveDungeon(saveFileName); // Pass the filename to saveDungeon()
+        } else {
+            printf("Error: No file path specified for saving!\n");
+            return 1;
+        }
     }
 
     return 0;
