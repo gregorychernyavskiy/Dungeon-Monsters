4289e6d103c36f980e1d740d975039c093d840f5
gregorychernyavskiy <gregory.chernyavskiy@gmail.com>
2025-02-16 13:59:21 -0600

    .

--- save_load.c
+++ save_load.c
@@ -6,30 +6,46 @@
 
 #include "dungeon_generation.h"
 
-char *dungeon_file;
-int upStairsCount, downStairsCount;
-struct Room *upStairs, *downStairs;
-
-void setupDungeonFile(char *filename) {
-    char *home = getenv("HOME");
-    int dungeon_file_length = strlen(home) + strlen("/.rlg327/") + strlen(filename) + 1;
-
-    dungeon_file = malloc(dungeon_file_length * sizeof(*dungeon_file));
-    if (!dungeon_file) {
-        perror("Memory allocation failed for dungeon_file");
+void saveDungeon() {
+    FILE *file = fopen("dungeon", "w");
+    if (!file) {
+        perror("Error opening file for saving");
         exit(EXIT_FAILURE);
     }
 
-    strcpy(dungeon_file, home);
-    strcat(dungeon_file, "/.rlg327/");
-    strcat(dungeon_file, filename);
+    fwrite("RLG327-S2025", 1, 12, file);
+
+    uint32_t version = htobe32(0);
+    fwrite(&version, 4, 1, file);
+
+    uint32_t size = htobe32(1712 + num_rooms * 4);
+    fwrite(&size, 4, 1, file);
+
+    uint8_t pos[2] = {(uint8_t) player_x, (uint8_t) player_y};
+    fwrite(pos, 2, 1, file);
+
+    fwrite(hardness, sizeof(hardness), 1, file);
+    
+    uint16_t r = htobe16(num_rooms);
+    fwrite(&r, 2, 1, file);
+    fwrite(rooms, sizeof(struct Room), num_rooms, file);
+
+    uint16_t u = htobe16(upStairsCount);
+    fwrite(&u, 2, 1, file);
+    fwrite(upStairs, sizeof(upStairs), upStairsCount, file);
+
+    uint16_t d = htobe16(downStairsCount);
+    fwrite(&d, 2, 1, file);
+    fwrite(downStairs, sizeof(downStairs), downStairsCount, file);
+
+    printf("Dungeon saved successfully.\n");
+    fclose(file);
 }
 
-void loadDungeon(char *filename) {
-    setupDungeonFile(filename);
-    FILE *file = fopen(dungeon_file, "r");
+void loadDungeon() {
+    FILE *file = fopen("dungeon", "r");
     if (!file) {
-        perror("Error opening file");
+        perror("Error opening file for loading");
         exit(EXIT_FAILURE);
     }
 
@@ -58,66 +74,21 @@ void loadDungeon(char *filename) {
     
     uint16_t r;
     fread(&r, 2, 1, file);
-    r = be16toh(r);
-    num_rooms = r;
-
-    fread(rooms, sizeof(struct Room), r, file);
+    num_rooms = be16toh(r);
+    fread(rooms, sizeof(struct Room), num_rooms, file);
     
     uint16_t u;
     fread(&u, 2, 1, file);
-    u = be16toh(u);
-    upStairsCount = u;
-    upStairs = malloc(u * sizeof(struct Room));
-    fread(upStairs, sizeof(struct Room), u, file);
+    upStairsCount = be16toh(u);
+    fread(upStairs, sizeof(upStairs), upStairsCount, file);
     
     uint16_t d;
     fread(&d, 2, 1, file);
-    d = be16toh(d);
-    downStairsCount = d;
-    downStairs = malloc(d * sizeof(struct Room));
-    fread(downStairs, sizeof(struct Room), d, file);
+    downStairsCount = be16toh(d);
+    fread(downStairs, sizeof(downStairs), downStairsCount, file);
     
     dungeon[player_y][player_x] = '@';
 
-    printf("Dungeon loaded from %s\n", dungeon_file);
+    printf("Dungeon loaded successfully.\n");
     fclose(file);
-    free(dungeon_file);
-}
-
-void saveDungeon(char *filename) {
-    setupDungeonFile(filename);
-    FILE *file = fopen(dungeon_file, "w");
-    if (!file) {
-        perror("Error opening file");
-        exit(EXIT_FAILURE);
-    }
-
-    fwrite("RLG327-S2025", 1, 12, file);
-
-    uint32_t version = htobe32(0);
-    fwrite(&version, 4, 1, file);
-
-    uint32_t size = htobe32(sizeof(1712 + num_rooms * 4));
-    fwrite(&size, 4, 1, file);
-
-    uint8_t pos[2] = {(uint8_t) player_x, (uint8_t) player_y};
-    fwrite(pos, 2, 1, file);
-
-    fwrite(hardness, sizeof(hardness), 1, file);
-    
-    uint16_t r = htobe16(num_rooms);
-    fwrite(&r, 2, 1, file);
-    fwrite(rooms, sizeof(struct Room), num_rooms, file);
-
-    uint16_t u = htobe16(upStairsCount);
-    fwrite(&u, 2, 1, file);
-    fwrite(upStairs, sizeof(struct Room), upStairsCount, file);
-
-    uint16_t d = htobe16(downStairsCount);
-    fwrite(&d, 2, 1, file);
-    fwrite(downStairs, sizeof(struct Room), downStairsCount, file);
-
-    printf("Dungeon saved to %s\n", dungeon_file);
-    fclose(file);
-    free(dungeon_file);
 }
